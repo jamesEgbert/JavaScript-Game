38fadbc7853968e9a421569016370c38c72dd493
fdono <45469257+fdono@users.noreply.github.com>
2018-12-11 14:36:28 -0800

    Update goblinquest.js

--- goblinquest.js
+++ goblinquest.js
@@ -2,40 +2,50 @@
 
 // Create the canvas
 // Set variables for use with methods
+//James made the function below me
+var continu;
+var monsterFlee;
+var userFlee;
+// user input function
+function action() {
+    var turn = "";
+    var turn = prompt("Its your turn buddy!").toLowerCase;
+    return turn;
+}
+//james also made this one wow he's so good
+// the monsters turn
+function monsterAction() {
+    var monsterTurn =  "";
+    var monsterTurn = cpu("attack", "defend", "flee").toLowerCase;
+}
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 //use canvas var to set dimensions
-canvas.width = 1366;
-canvas.height = 768;
-canvas.textAlign = "center";
+canvas.width = 512;
+canvas.height = 480;
 //append body element with canvas var above
 document.body.appendChild(canvas);
-
-var GRAVITY = 256;
-var maxBullets = 5;
 
 // Background image
 // use boolean toggle to set image ready var to false
 var bgReady = false;
 //creates new image object and stores it in bgImage
-var bgImage = new Image(1366, 768);
+var bgImage = new Image();
 
 //tests to see when image is loaded and sets image ready to true
 bgImage.onload = function () {
 	bgReady = true;
 };
 //sets image source
-bgImage.src = "../_images/background_large.png";
-bgImage.width = 1366;
-bgImage.height = 768;
+bgImage.src = "../_images/background.png";
 
 // Hero image
-var HeroReady = false;
-var HeroImage = new Image();
-HeroImage.onload = function () {
-	HeroReady = true;
+var heroReady = false;
+var heroImage = new Image();
+heroImage.onload = function () {
+	heroReady = true;
 };
-HeroImage.src = "../_images/warrior.png";
+heroImage.src = "../_images/warrior.png";
 
 // Monster image
 var monsterReady = false;
@@ -45,261 +55,96 @@ monsterImage.onload = function () {
 };
 monsterImage.src = "../_images/goblin.png";
 
-// Bullet image
-var bulletReady = false;
-var bulletImage = new Image();
-bulletImage.onload = function () {
-	bulletReady = true;
+// Monster2 image
+var monster2Ready = false;
+var monster2Image = new Image();
+monster2Image.onload = function () {
+	monster2Ready = true;
 };
-bulletImage.src = "../_images/arrow.png";
+monster2Image.src = "../_images/bokoblin.png";
 
 // Game objects
-var Hero = {
+var hero = {
 	speed: 256, // movement in pixels per second
 	x: canvas.width / 2,
-	y: canvas.height - 128,
-	gravity: GRAVITY,
-	shoot: function () {
-		console.log("I'm shooting!");
-	},
-	jump: function () {
-		this.y -= 64;
-	}
+    y: canvas.height / 2
 };
-
-// monster object
-//var Monster = {
-//	speed: 256, // movement in pixels per second
-//	x: canvas.width / 2,
-//	y: canvas.height / 2,
-//	gravity: GRAVITY,
-//	shoot: function() {
-//        console.log("I'm shooting!");
-//    },
-//	jump: function() {
-//        Hero.y -= 64;
-//    }
-//};
-
-function getRect(image) {
-  var rect = image.getBoundingClientRect();
-  x = rect.left;
-  y = rect.top;
-  w = rect.width;
-  h = rect.height;
-  console.log("Left: " + x + ", Top: " + y + ", Width: " + w + ", Height: " + h);
-}
-
-// all monster array (empty toy box)
-var allMonsters = [];
-
-// Constructor function for Monster objects
-
-function Monster() {
-	this.x = 64 + (Math.random() * (canvas.width - 128)); 
-	this.y = 64 + 64 + (Math.random() * (canvas.height/2));
-	this.health = 100;
-	this.speed = 256; 
-	this.gravity = GRAVITY; 
-	this.jump = function(){
-		this.y -= Math.random()*256;
-	};
-	this.reset = function() {
-		this.x = 128 + (Math.random() * (canvas.width - 128));
-		this.y = 128 + (Math.random() * (canvas.height/2));
-	};
-
-	allMonsters.push(this);
-	console.log(allMonsters);
-}
-
-
-function range(start, end) {
-    if(start === end) return [start];
-    return [start, ...range(start + 1, end)];
-}
-
-
-for (monster in range(0,5)){
-	monster = new Monster();
-}
-
-var allBullets = [];
-function Bullet() {
-	this.x = Hero.x;
-	this.y = Hero.y;
-	this.speed = 256;
-	allBullets.push(this);
-	console.log(allBullets);
-}
-
+var monster = {};
+var monster2 = {};
 var monstersCaught = 0;
 
 // Handle keyboard controls
-//.keyCode is deprecated
-//use https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key
-
 var keysDown = {};
 
-addEventListener("keypress", function (e) {
-	keysDown[e.key] = true;
+addEventListener("keydown", function (e) {
+	keysDown[e.keyCode] = true;
 }, false);
 
 addEventListener("keyup", function (e) {
-	delete keysDown[e.key];
+	delete keysDown[e.keyCode];
 }, false);
 
-// Start the 
-//var startGame = function() {
-//	for (monster in allMonsters) {
-//		allMonsters[monster].x = 64 + (Math.random() * (canvas.width - 128));
-//		allMonsters[monster].y = 64 + (Math.random() * (canvas.height - 128));
-//		
-//	}
-//}
+// Reset the game when the player catches a monster
+var reset = function () {
+//	hero.x = canvas.width / 2;
+//	hero.y = canvas.height / 2;
+
+	// Throw the monster somewhere on the screen randomly
+	monster.x = 64 + (Math.random() * (canvas.width - 128));
+	monster.y = 64 + (Math.random() * (canvas.height - 128));
+	console.log(monster.x);
+	console.log(monster.y);
+	monster2.x = 64 + (Math.random() * (canvas.width - 128));
+	monster2.y = 64 + (Math.random() * (canvas.height - 128));
+	console.log(monster2.x);
+	console.log(monster2.y);
+};
+
 // Update game objects
-var delay = 500;
-var ticks = 0;
 var update = function (modifier) {
-		var now = Date.now();
-		var delta = (now - oldTimeStamp);
-		ticks += delta;
-		// console.log(ticks);
-		if ("w" in keysDown) { // Player holding up
-//			Hero.y -= Hero.speed * modifier;
-			console.log("change in time is " + delta);
-			if (allBullets.length < maxBullets && ticks > delay ){
-				bullet = new Bullet();
-				ticks = 0;
-				console.log(allBullets);
-			}
-		}
-		then = now;
-		if ("s" in keysDown) { // Player holding down
-			Hero.y += Hero.speed * modifier;
-			console.log(Hero.x, Hero.y);
-		}
-		if ("a" in keysDown) { // Player holding left
-			Hero.x -= Hero.speed * modifier;
-			console.log(Hero.x, Hero.y);
-		}
-		if ("d" in keysDown) { // Player holding right
-			Hero.x += Hero.speed * modifier;
-			console.log(Hero.x, Hero.y);
-		}
-//		if (Hero.y <= canvas.height - 128) {
-//			Hero.y += Hero.gravity;
-//		}
-		if (" " in keysDown) { // Player jumps
-
-			if (Hero.y > canvas.height - 128) {
-				console.log(Hero.y);
-				Hero.jump();
-			}
-//			console.log(Hero.y > canvas.height);
-		}
-		
-		for (bullet in allBullets) {
-			allBullets[bullet].y -= allBullets[bullet].speed*modifier;
-			console.log(allBullets[bullet].y);
-			if (allBullets[bullet].y < 0) {
-					allBullets.splice(bullet, 1);
-
-			}
-		}
-		for (monster in allMonsters) {
-//			console.log(monster.gravity + 'monster gravity');
-			if (allMonsters[monster].y <= canvas.height) {
-				allMonsters[monster].y += allMonsters[monster].gravity*modifier;
-			
-			
-			}
-			if (allMonsters[monster].y > canvas.height - 128) {
-//				console.log(Hero.y);
-				allMonsters[monster].jump();
-			}
-		}
-	// Are they touching? This is called a collision
-	for (monster in allMonsters) {
-//		console.log("looking for collisions...");
-//		console.log("hero collision " + Hero.x);
-//		console.log("monster collision " + m.x);
-		
-
-		for (bullet in allBullets) {
-			if (
-				allBullets[bullet].x <= (allMonsters[monster].x + 64) &&
-				allMonsters[monster].x <= (allBullets[bullet].x + 64) &&
-				allBullets[bullet].y <= (allMonsters[monster].y + 64) &&
-				allMonsters[monster].y <= (allBullets[bullet].y + 64)
-			){
-				++monstersCaught;
-	//			allMonsters[monster].reset();
-				allMonsters.splice(monster, 1);
-				allBullets.splice(bullet, 1);
-				console.log("collision");
-				console.log(allMonsters)
-	//			monster = new Monster();
-				console.log(allMonsters);
-			
-			}
-		}
-		if (
-			Hero.x <= (allMonsters[monster].x + 64) &&
-			allMonsters[monster].x <= (Hero.x + 64) &&
-			Hero.y <= (allMonsters[monster].y + 64) &&
-			allMonsters[monster].y <= (Hero.y + 64)
-		) {
-			++monstersCaught;
-//			allMonsters[monster].reset();
-			allMonsters.splice(monster, 1);
-			console.log("collision");
-			console.log(allMonsters)
-//			monster = new Monster();
-			console.log(allMonsters);
-			
-			
-		}
+	if (87 in keysDown) { // Player holding up
+		hero.y -= hero.speed * modifier;
+	}
+	if (83 in keysDown) { // Player holding down
+		hero.y += hero.speed * modifier;
+	}
+	if (65 in keysDown) { // Player holding left
+		hero.x -= hero.speed * modifier;
+	}
+	if (68 in keysDown) { // Player holding right
+		hero.x += hero.speed * modifier;
 	}
 
-
-	} // end of update
-
-
-
-
+	// Are they touching?
+	if (
+		hero.x >= (monster.x - 32)
+		&& monster.x >= (hero.x - 32)
+		&& hero.y >= (monster.y - 32)
+		&& monster.y >= (hero.y - 32)
+	) {
+		
+	}
+};
 
 // Draw everything
 var render = function () {
-
-	//	https://developer.mozill	a.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
+	
+//	https://developer.mozill	a.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
 	if (bgReady) {
 		ctx.drawImage(bgImage, 0, 0);
 	}
-	
+
+	if (heroReady) {
+		ctx.drawImage(heroImage, hero.x, hero.y);
+	}
+
 	if (monsterReady) {
-		for (monster in allMonsters) {
-			ctx.drawImage(monsterImage, allMonsters[monster].x, allMonsters[monster].y);
-
-		}
+		ctx.drawImage(monsterImage, monster.x, monster.y);
 	}
 	
-	
-
-	if (HeroReady) {
-		ctx.drawImage(HeroImage, Hero.x, Hero.y);
+	if (monster2Ready) {
+		ctx.drawImage(monster2Image, monster2.x, monster2.y);
 	}
-	if (bulletReady) {
-	
-	for (bullet in allBullets) {
-		ctx.drawImage(bulletImage, allBullets[bullet].x, allBullets[bullet].y);
-
-	}
-	}
-	
-	
-
-
 
 	// Score
 	ctx.fillStyle = "rgb(250, 250, 250)";
@@ -311,19 +156,86 @@ var render = function () {
 
 // The main game loop
 // The Date.now() method returns the number of milliseconds elapsed since January 1, 1970 00:00:00 UTC.
+//James made the combat system wowowowow
 var main = function () {
-	//	creates (and resets each call) a new timestamp when the main func is called
-
+	var userHealth = 20;
+    var monsterHealth = 20;
+    var playerVictory = false;
+    var battling = true;
+    while (battling == true) {
+    //The loop of life is above me :O
+        action();
+	//Ask the user what they want to do
+        monsterAction(); 
+	//Random number of what the monster wants to do
+        if (turn == "attack") {
+            if (monsterTurn == "defend") {
+                console.log("The user used attack, but the monster defended, so no damage taken!");
+				heatlh
+            }
+            if (monsterTurn == "attack") {
+                console.log("The user used attack, and the monster takes 5 damage!");
+            }
+        }
+	//Case scenarios in which the user uses attack
+        if (turn == "defend") {
+            if (monsterTurn == "attack") {
+                console.log("The user defends, and the monster attacks. No damage is taken!");
+            }
+            if (monsterTurn == "defend") {
+                console.log("The user and monster defend, you both look stupid.");
+            }
+            if (monsterTurn == "flee") {
+                if (userFlee < monsterFlee) {
+                    console.log("The monster flees and does so successfully!");
+                    battling = false;
+                }
+                if (speed >= monsterFlee) {
+                    console.log("The monster tries to flee, but it does so unsecesfully!");
+                }
+            }
+        }
+	//Another case scenario in which the user uses defend
+        if (turn == "flee") {
+            if (speed >= monsterFlee) {
+                console.log("The user sucessfully escpaes!");
+                battling = false;
+            }
+            if (speed < monsterFlee) {
+                console.log("The user isn't fast enough to escape!");
+                if (monsterTurn == "attack") {
+                    console.log("The user tries to flee, but isn't fast enought, and the monster attacks. -5 health");
+                }
+                if (monsterTurn == "defend") {
+                    console.log("The user tries to flee, but isn't fast enough, the monster uses flee");
+                }
+                if (monsterTurn == "flee") {
+                    console.log("The user tries to flee, but isn't fast enought, luckily the monster flees and escapes successfully. So uh good job?");
+                    battling = false;
+                }
+            }
+        }
+	//You might have been guessing it here it is the final case scenario in which the user uses flee :O
+    }
+    continu = "d";
+    continu = prompt("Type any button to continue.");
+    //So the user can seperate turns
+//	creates (and resets each call) a new timestamp when the main func is called
 	var newTimeStamp = Date.now();
-	//	records time difference from line before main is called and line above in miliseconds
+//	records time difference from line before main is called and line above in miliseconds
 	var delta = newTimeStamp - oldTimeStamp;
-	// runs update function passing milliseconds of time passed divided by 1K
-
+// runs update function passing milliseconds of time passed divided by 1K
+	console.log(oldTimeStamp);
+	console.log(newTimeStamp);
+	console.log(delta);
 	update(delta / 1000);
-	//	renders content
+//	renders content
 	render();
-	// sets oldTimeStamp to newTimeStamp
+// sets oldTimeStamp to newTimeStamp
 	oldTimeStamp = newTimeStamp;
+	
+	
+
 	// Request to do this again ASAP
 	requestAnimationFrame(main);
 };
@@ -334,6 +246,5 @@ requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame
 
 // Let's play this game!
 var oldTimeStamp = Date.now();
-//startGame();
-
+reset();
 main();
